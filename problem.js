4659db2ea57b9d4a9d248777c460cf4f1afb1963
Jivak Sute <jivakysute@gmail.com>
2024-01-10 11:25:28 +0530

    js practice

--- problem.js
+++ problem.js
@@ -1,5 +1,13 @@
-async function getdata() {
-  return await Promise.resolve("I made It");
+// async function getdata() {
+//   return await Promise.resolve("I made It");
+// }
+// const data = getdata();
+// console.log(data);
+
+function print1() {
+  return console.log("Hello");
 }
-const data = getdata();
-console.log(data);
+function print2(x) {
+  return x;
+}
+console.log(print1() && print2(55));
