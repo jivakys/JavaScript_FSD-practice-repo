7fb3428f10cb239432f4629185326f6bdb318fb8
Jivak Sute <jivakysute@gmail.com>
2024-01-16 08:37:39 +0530

    order route clear

--- index.js
+++ index.js
@@ -2213,3 +2213,17 @@ function increment() {
 const inc = increment();
 inc();
 inc();
+
+async function getdata() {
+  return await Promise.resolve("I made It");
+}
+const data = getdata();
+console.log(data);
+
+function print1() {
+  return console.log("Hello");
+}
+function print2(x) {
+  return x;
+}
+console.log(print1() && print2(55));
