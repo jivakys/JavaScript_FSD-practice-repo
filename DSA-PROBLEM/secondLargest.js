9ffc1fea09c9c46653b824d4747537043f87d2f8
Jivak Sute <jivakysute@gmail.com>
2024-04-04 17:21:55 +0530

    js practice

---
+++ DSA-PROBLEM/secondLargest.js
@@ -0,0 +1,16 @@
+function findSecLarge(arr) {
+  let largest = arr[0];
+  let secondLarge = -Infinity;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      secondLarge = largest;
+      largest = arr[i];
+    } else if (arr[i] < largest && arr[i] > secondLarge) {
+      secondLarge = arr[i];
+    }
+  }
+  console.log(secondLarge);
+}
+
+let arr = [1, 2, 3, 4, 5, 5, 6, 8, 4];
+findSecLarge(arr);
